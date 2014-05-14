0d003e6e0e5de5d74eea0020911d5e28addca5f3
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2014-05-15 01:37:57 +0800

    Deleted unused files
    Deleted unused files

--- app/views/ace/dashboard.blade.php
+++
@@ -1,5 +0,0 @@
-@section('content')
-
-@include($theme.'.includes.current_subjects_enrolled', array('current_subjects_enrolled', $current_subjects_enrolled))
-
-@stop
--- app/views/ace/proposed-list.blade.php
+++
@@ -1,5 +0,0 @@
-@section('content')
-
-@include($theme.'.includes.proposed_list', array('proposed_list', $proposed_list))
-
-@stop
